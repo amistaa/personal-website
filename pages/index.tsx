bb66c4898015859f00b5257ed5a00e5609246b34
Med <62178839+amistaa@users.noreply.github.com>
2021-10-10 21:45:07 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,115 @@
+// import Link from "next/link";
+// import Head from "next/head";
+
+// const App = () => {
+//   return (
+//     <div
+//       className="bg-cover bg-center"
+//       style={{
+//         backgroundImage: "url(./background.jpg)",
+//       }}
+//     >
+//       <Head>
+//         <title>Amista | Personal Website</title>
+//         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+//       </Head>
+//       <div className="flex flex-col items-center justify-center h-screen">
+//         <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2 ">
+//           <article className="overflow-hidden rounded-lg shadow-lg bg-white">
+//             <img
+//               alt="Wallpaper"
+//               src="./header.jpg"
+//               className="block h-auto w-full"
+//             />
+//             <header className="flex items-center justify-around leading-tight p-2 md:p-4">
+//               <a href="https://github.com/amistaa">
+//                 <img
+//                   alt="Github"
+//                   src="./github.png"
+//                   className="block h-auto w-8 md:w-16 hover:opacity-50 cursor-pointer"
+//                 />
+//               </a>
+//               <a href="https://discord.com/users/666295012522459136/">
+//                 <img
+//                   alt="Discord"
+//                   src="./discord.png"
+//                   className="block h-auto w-5 md:w-10 hover:opacity-50 cursor-pointer"
+//                 />
+//               </a>
+//               <a href="https://facebook.com/AmistaDev">
+//                 <img
+//                   alt="Facebook"
+//                   src="./facebook.png"
+//                   className="block h-auto w-5 md:w-10 hover:opacity-50 cursor-pointer"
+//                 />
+//               </a>
+//               <a href="https://weibo.com/7536810146">
+//                 <img
+//                   alt="Weibo"
+//                   src="./weibo.png"
+//                   className="block h-auto w-7 md:w-12 hover:opacity-50 cursor-pointer"
+//                 />
+//               </a>
+//               <a href="https://osu.ppy.sh/users/Amista">
+//                 <img
+//                   alt="osu!"
+//                   src="./osu.png"
+//                   className="block h-auto w-7 md:w-12 hover:opacity-50 cursor-pointer"
+//                 />
+//               </a>
+//             </header>
+//             <div className="border-b-2" />
+//             <footer className="flex items-center justify-around leading-none p-2 md:p-4">
+//               <Link href="/Project">
+//                 <button className="md:w-32 bg-white border-2 border-blue-300 hover:bg-blue-dark text-blue-300 font-bold py-3 px-6 rounded-lg hover:bg-blue-300 hover:shadow-md hover:text-white transition ease-in-out duration-300">
+//                   Project
+//                 </button>
+//               </Link>
+//             </footer>
+//           </article>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import ProjectAPI from "./api/ProjectAPI.json";
 import Link from "next/link";
 import Head from "next/head";
 
-const App = () => {
+const Project = () => {
   return (
-    <div
-      className="bg-cover bg-center"
-      style={{
-        backgroundImage: "url(./background.jpg)",
-      }}
-    >
+    <>
       <Head>
-        <title>Amista | Personal Website</title>
+        <title>Amista | Project</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2 ">
-          <article className="overflow-hidden rounded-lg shadow-lg bg-white">
-            <img
-              alt="Wallpaper"
-              src="./header.jpg"
-              className="block h-auto w-full"
-            />
-            <header className="flex items-center justify-around leading-tight p-2 md:p-4">
-              <a href="https://github.com/amistaa">
-                <img
-                  alt="Github"
-                  src="./github.png"
-                  className="block h-auto w-8 md:w-16 hover:opacity-50 cursor-pointer"
-                />
-              </a>
-              <a href="https://discord.com/users/666295012522459136/">
-                <img
-                  alt="Discord"
-                  src="./discord.png"
-                  className="block h-auto w-5 md:w-10 hover:opacity-50 cursor-pointer"
-                />
-              </a>
-              <a href="https://facebook.com/AmistaDev">
-                <img
-                  alt="Facebook"
-                  src="./facebook.png"
-                  className="block h-auto w-5 md:w-10 hover:opacity-50 cursor-pointer"
-                />
-              </a>
-              <a href="https://weibo.com/7536810146">
-                <img
-                  alt="Weibo"
-                  src="./weibo.png"
-                  className="block h-auto w-7 md:w-12 hover:opacity-50 cursor-pointer"
-                />
-              </a>
-              <a href="https://osu.ppy.sh/users/Amista">
-                <img
-                  alt="osu!"
-                  src="./osu.png"
-                  className="block h-auto w-7 md:w-12 hover:opacity-50 cursor-pointer"
-                />
-              </a>
-            </header>
-            <div className="border-b-2" />
-            <footer className="flex items-center justify-around leading-none p-2 md:p-4">
-              <Link href="/Project">
-                <button className="md:w-32 bg-white border-2 border-blue-300 hover:bg-blue-dark text-blue-300 font-bold py-3 px-6 rounded-lg hover:bg-blue-300 hover:shadow-md hover:text-white transition ease-in-out duration-300">
-                  Project
-                </button>
-              </Link>
-            </footer>
-          </article>
-        </div>
+      <div className="fixed flex w-full bg-white border-b items-center justify-between flex-wrap p-4 m-auto top-0 animated z-10">
       </div>
-    </div>
+      <div className="mt-10 p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        {ProjectAPI.map((data: any) => {
+          return (
+            <div className="rounded overflow-hidden shadow-lg hover:opacity-75">
+              <a href={data.url}>
+                <img className="w-full" src={data.image} alt={data.name} />
+                <div className="px-6 py-4">
+                  <div className="flex text-md mb-2 justify-between">
+                    <p>{data.name}</p>
+                    <p className="font-bold text-green-500">{data.date}</p>
+                  </div>
+                  <p className="text-gray-700 text-base mt-5 text-justify">
+                    {data.desc}
+                  </p>
+                </div>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
-export default App;
+export default Project;
